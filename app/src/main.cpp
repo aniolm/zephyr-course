8593e145f262e73e385267b5f3e01494c8b844a1
aniolm <aniolm@o2.pl>
2026-05-03 15:37:01 +0000

    Add custom Kconfig file

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,8 +2,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SLEEP_TIME_MS 1000
-
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
 
@@ -11,20 +9,80 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+static int get_blink_sleep_ms(void)
+{
+#ifdef CONFIG_LED_BLINK_SLEEP_MS
+    return CONFIG_LED_BLINK_SLEEP_MS;
+#else
+    return 1000;
+#endif
+}
+
 int main(void)
 {
     bool led_state = true;
 
-    if (!gpio_is_ready_dt(&led)) return 0;
+#ifdef CONFIG_LED_BRIGHTNESS
+    const int brightness = CONFIG_LED_BRIGHTNESS;
+#else
+    const int brightness = 100;
+#endif
 
-    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+#ifdef CONFIG_LED_FADE_DURATION_MS
+    const int fade_duration_ms = CONFIG_LED_FADE_DURATION_MS;
+#else
+    const int fade_duration_ms = 0;
+#endif
+
+#ifdef CONFIG_LED_DEBUGGING
+    const bool led_debug_enabled = true;
+#else
+    const bool led_debug_enabled = false;
+#endif
+
+#ifdef CONFIG_LED_CUSTOM_BLINK_PATTERN
+    const bool custom_pattern_enabled = true;
+#else
+    const bool custom_pattern_enabled = false;
+#endif
+
+    const int blink_sleep_ms = get_blink_sleep_ms();
+
+    LOG_INF("LED config: blink=%dms, brightness=%d, fade=%dms, debug=%d, custom_pattern=%d",
+            blink_sleep_ms, brightness, fade_duration_ms,
+            led_debug_enabled ? 1 : 0, custom_pattern_enabled ? 1 : 0);
+
+    if (!gpio_is_ready_dt(&led)) {
+        LOG_ERR("LED GPIO is not ready");
+        return 0;
+    }
+
+    if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) {
+        LOG_ERR("Failed to configure LED GPIO");
+        return 0;
+    }
 
     while (1) {
-        if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (gpio_pin_toggle_dt(&led) < 0) {
+            LOG_ERR("Failed to toggle LED GPIO");
+            return 0;
+        }
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+
+        if (led_debug_enabled) {
+            LOG_INF("debug: sleep=%dms brightness=%d fade=%dms", blink_sleep_ms, brightness,
+                    fade_duration_ms);
+        }
+
+        if (custom_pattern_enabled) {
+            k_msleep(blink_sleep_ms / 2);
+            continue;
+        }
+
+        k_msleep(blink_sleep_ms);
     }
+
     return 0;
 }
